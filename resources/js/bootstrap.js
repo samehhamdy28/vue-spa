d69d9e1b9f482cc3268ba0a78c57bb6079f36a09
Sameh <sameh.hamdy@roqay.com>
2022-08-09 22:49:43 +0200

    first commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -15,8 +15,8 @@ import 'bootstrap';
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.withCredentials = true;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true; //instruct axios to send authentication cookie with every request
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'; //instruct axios to send X-Requested-With header with every request
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
